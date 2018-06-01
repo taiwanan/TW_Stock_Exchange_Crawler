e2037bfa311d47c139e73266a9b304d9315c05b9
taiwanan <g9002012@gmail.com>
2018-06-01 11:09:34 +0800

    modify run.sh - auto fetch TSE/TWSE_IDX data every month

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-cd /home/asoul/tsec/
-/usr/bin/python crawl.py --check
-/usr/bin/python post_process.py
-/usr/bin/git add .
-/usr/bin/git commit -m "daily update"
-/usr/bin/git push
+today=$(date '+%Y/%m')
+commit="TSE raw data/TWSE_IDX update to $today" 
+python -u fetch_tse.py
+python -u fetch_twse_idx.py
+git add TWSE_IDX.csv twse_momentum_table.xlsx tse_raw_data/.
+git commit -m "$commit"
+#/usr/bin/git push
