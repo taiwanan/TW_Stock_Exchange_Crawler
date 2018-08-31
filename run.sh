a2bbeed762994d25d0ffa438932a5d8754679211
taiwanan <g9002012@gmail.com>
2018-08-31 14:40:01 +0800

    TSE/OTC raw data & TWSE_IDX update to 2018/08
    add new TSE stock [00743] 國泰中國A150
    add new TSE stock [2882B] 國泰金乙特
    add new TSE stock [4540] 全球傳動
    add new TSE stock [8482] 商億-KY
    add new OTC stock [1240] 茂生農經

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 today=$(date '+%Y/%m')
-commit="TSE raw data/TWSE_IDX update to $today" 
+commit="TSE/OTC raw data & TWSE_IDX update to $today"
 python -u fetch_trading.py
 python -u fetch_twse_idx.py
 git add TWSE_IDX.csv twse_momentum_table.xlsx tse_trading_raw_data/. otc_trading_raw_data/.
